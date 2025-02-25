8c4dac4fac83995be1638d4bb78020be327ebee2
Ranit <Ranit@DESKTOP-P418K1F>
2025-02-25 17:24:57 +0300

    add logging and WeatherForecast test request

--- RanitSabitovKt-41-22/obj/Debug/net8.0/RanitSabitovKt-41-22.MvcApplicationPartsAssemblyInfo.cs
+++ RanitSabitovKt-41-22/obj/Debug/net8.0/RanitSabitovKt-41-22.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("RanitSabitovKt-41-22")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fed1e24592c96a70176694b77c91ffd4c1860245")]
-[assembly: System.Reflection.AssemblyProductAttribute("RanitSabitovKt-41-22")]
-[assembly: System.Reflection.AssemblyTitleAttribute("RanitSabitovKt-41-22")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Создано классом WriteCodeFragment MSBuild.
 
